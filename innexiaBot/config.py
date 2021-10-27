172229979a415dddf18aef546b0d66587079957a
looserakki <70113298+looserakki@users.noreply.github.com>
2021-10-27 23:06:01 +0530

    Update config.py

--- innexiaBot/config.py
+++ innexiaBot/config.py
@@ -12,6 +12,7 @@ class Development(Config):
     EVENT_LOGS = "-1001504294786"
     REDIS_URL = "redis-18987.c16.us-east-1-2.ec2.cloud.redislabs.com:18987"
     STRICT_GBAN = "True"
+    ALLOW_CHATS = "True"
     BOT_ID = "1728730929"
     API_ID = "3546656"
     API_HASH = "48b79c54af688f05c350161bddea414c"
