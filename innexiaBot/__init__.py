0d50046ab60c6f06111b5389aadea4c2bf1bfae8
looserakki <70113298+looserakki@users.noreply.github.com>
2021-10-28 07:49:38 +0530

    Update __init__.py

--- innexiaBot/__init__.py
+++ innexiaBot/__init__.py
@@ -147,6 +147,7 @@ else:
     DONATION_LINK = Config.DONATION_LINK
     LOAD = Config.LOAD
     NO_LOAD = Config.NO_LOAD
+    WORKERS = Config.WORKERS
     STRICT_GBAN = Config.STRICT_GBAN
     BAN_STICKER = Config.BAN_STICKER
     ALLOW_EXCL = Config.ALLOW_EXCL
